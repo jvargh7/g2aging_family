a77ebfc3bd581d66362605ea3d86659e95ee7397
Varghese <JVARGH7@emory.edu>
2022-11-30 09:08:01 -0500

    Updated figure for statistical challenges in couple concordance

--- paper/figure_statistical challenges.R
+++ paper/figure_statistical challenges.R
@@ -1,78 +1,52 @@
+# The figure attempts to prove 2 points
+# 1. As marginal prevalence increases, minimum joint prevalence for large magnitude associations increases?
+# 2. For similar marginal prevalences, PR is symmetric for wives and husbands --> This doesn't need proving as such
 
-pW = seq(5,100,by=5)
-pH = seq(5,100,by=5)
+pW = seq(0.05,1,by=0.05)
+pH = seq(0.05,1,by=0.05)
 
-prop_WpHp = seq(0,1,by=0.05)
+PR = seq(1,1.5,by=0.1)
 
-PR_df <- expand.grid(pW,pH,prop_WpHp) %>% 
-  data.frame() %>%
+# PR_{wife} = pWpHp/pH / pWpHn/(1-pH)
+# prob_WpHp = pWpHp/pH
+
+prob_WpHp = seq(0,1,by=0.05)
+
+PR_wife = expand.grid(pW,pH,prob_WpHp) %>% 
+  data.frame() %>% 
   rename(pW = Var1,
          pH = Var2,
-         prop_WpHp = Var3) %>% 
-  dplyr::mutate(pWpHp = pmin(pW*prop_WpHp,pH*prop_WpHp)
-         ) %>% 
-  dplyr::filter(!is.na(pWpHp)) %>% 
-  mutate(pWpHn = pW - pWpHp,
-         pWnHp = pH - pWpHp) %>% 
-  mutate(PR_wife = (pWpHp/pH)/(pWpHn/(100-pH)),
-         PR_husband = (pWpHp/pW)/(pWnHp/(100-pW)),
-         ) %>% 
-  # dplyr::filter(PR_wife > 1) %>%
-  # group_by(pW,pH) %>% 
-  # summarize(PR_wife = median(PR_wife),
-  #           PR_husband = median(PR_husband)) %>% 
-  # ungroup() %>% 
-  mutate(PR_category_wife = case_when(PR_wife <= 1 ~ 1,
-                                      PR_wife > 1 & PR_wife <= 1.1 ~ 2,
-                                      PR_wife > 1.1 & PR_wife <= 1.2 ~ 3,
-                                      TRUE ~ 4),
-         PR_category_husband = case_when(PR_husband <= 1 ~ 1,
-                                      PR_husband > 1 & PR_husband <= 1.1 ~ 2,
-                                      PR_husband > 1.1 & PR_husband <= 1.2 ~ 3,
-                                      TRUE ~ 4)
-         ) %>% 
-  mutate_at(vars(PR_category_wife,PR_category_husband),function(x) factor(x,levels=c(1:4),labels=c("<=1","1.01-1.10","1.11-1.20",">1.20")))
-
-figW <- ggplot(data=PR_df,aes(x = pW,y = pH,fill = PR_category_wife)) +
-  geom_tile() +
-  scale_fill_manual("Prevalence Ratio",values = c("darkgreen","yellow","orange","red")) +
-  theme_bw() +
-  xlab("Wife's marginal prevalence (%)") +
-  ylab("Husband's marginal prevalence (%)") +
-  theme(text = element_text(size = 20),
-        legend.position = "bottom")
-  
-figH <- ggplot(data=PR_df,aes(x = pW,y = pH,fill = PR_category_husband)) +
-  geom_tile() +
-  scale_fill_manual("Prevalence Ratio",values = c("darkgreen","yellow","orange","red")) +
-  theme_bw() +
-  xlab("Wife's marginal prevalence (%)") +
-  ylab("Husband's marginal prevalence (%)") +
-  theme(text = element_text(size = 20))
+         prob_WpHp = Var3) %>% 
+  mutate(pWpHp = pmin(pW,pH*prob_WpHp),
+         eWpHp = pH*pW,
+         pWpHn = pW - pWpHp,
+         p_minus_e = pWpHp - eWpHp) %>%
+  mutate(PR = round((pWpHp/pH)/(pWpHn/(1-pH)),1)) %>% 
+  dplyr::filter(p_minus_e > 0,PR < 2)
 
 
-require(ggpubr)
-# ggarrange(figW,figH,nrow=1,ncol=2,
-#           labels = c("Wife","Husband"),
-#           common.legend = TRUE,legend="bottom") %>% 
-#   ggsave(.,filename = paste0(path_g2a_family_folder,"/figures/figure_statistical challenge.png"),width=15,height=8)
+# For a given pH, what should be the value of pW for PR = 1, 1.1, 1.2,...,1.5
+require(plotly)
+# https://plotly.com/r/3d-mesh/
+fig <- PR_wife %>% 
+  mutate_at(vars(pW,pH,pWpHp,p_minus_e),~.*100) %>% 
+  plot_ly(data=.,x = ~pW,y=~pH,z=~p_minus_e,type="mesh3d",
+               intensity = ~PR,
+               colorscale = list(c(0,'grey'),
+                                 c(0.25,'lightgreen'),
+                                 c(0.50,'yellow'),
+                                 c(0.75, 'orange'),
+                                 c(1, 'red'))
+) %>%
+  layout(scene = list(xaxis = list(title = "Wife's prevalence (%)"), 
+                      yaxis = list(title = "Husband's prevalence (%)"),
+                      zaxis = list(title = "Joint - Expected Prevalence: Couple (%)")
+                      
+  )) %>% 
+  layout(legend=list(title=list(text='<b> Trend </b>')))
 
-figW %>% 
-  ggsave(.,filename = paste0(path_g2a_family_folder,"/figures/figure_statistical challenge.png"),width=8,height=8)
+fig
 
 
-# ----------------------
 
-# For different values of pWpH, what are the plots of PRs
 
-figW <- PR_df %>% 
-  group_by(PR_category_wife,pH,pW) %>% 
-  dplyr::filter(pWpHp == min(pWpHp))
-  ggplot(data=,aes(x = pW,y = pH,fill = PR_category_wife)) +
-  geom_tile() +
-  scale_fill_manual("Prevalence Ratio",values = c("darkgreen","yellow","orange","red")) +
-  theme_bw() +
-  xlab("Wife's marginal prevalence (%)") +
-  ylab("Husband's marginal prevalence (%)") +
-  theme(text = element_text(size = 20),
-        legend.position = "bottom")
